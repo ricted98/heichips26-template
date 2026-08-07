7af54530a445e56a55ef8a2c32318e30fa9ac118
aofarmakis <alfarmak@ad.inria.fr>
2026-08-07 11:29:30 +0200

    [wip] fix: PWM muting from staccato

--- macros/heichips26_daftASIC/rtl/pwm_driver.v
+++ macros/heichips26_daftASIC/rtl/pwm_driver.v
@@ -77,22 +77,15 @@ module pwm_driver (reset, clk, enable, valid, scancode, high_frequency_pwm_count
      reg [PWM_COUNTER_WIDTH-1:0] stac_max_count, stac_counter;
      reg [2:0] note_loaded;
      reg up_count, pwm_sample;
-     reg [7:0] valid_reg;
 
      wire [PWM_COUNTER_WIDTH-1:0] vib_downwards, vib_upwards;
      wire pwm_rose, note_off;
-
-     always @(posedge reset or posedge clk) begin
-          if (reset) valid_reg <= 1'b0;
-          else valid_reg <= valid;
-     end
-
 
      // Type of note loaded will decide the boundaries
      // of oscillation and the central frequency
      always @(posedge reset or posedge clk) begin
           if (reset) note_loaded <= 0;
-          else if (enable & valid_reg) begin
+          else if (enable & valid) begin
                case(scancode)
                     8'h23: begin                               // Character: D (Do)
                          note_loaded <= 1;
@@ -227,7 +220,7 @@ module pwm_driver (reset, clk, enable, valid, scancode, high_frequency_pwm_count
      always @(posedge reset or posedge clk) begin
           if (reset) stac_counter <= 0;
           else if (enable) begin
-               if (valid_reg) stac_counter <= 0;
+               if (valid) stac_counter <= 0;
                else if (staccato & pwm_rose) begin
                     if (note_off) stac_counter <= stac_counter;
                     else stac_counter <= stac_counter + 1;
@@ -241,13 +234,13 @@ module pwm_driver (reset, clk, enable, valid, scancode, high_frequency_pwm_count
                high_frequency_pwm_counter <= 0;
                up_count <= 1'b1;
           end
-          else if (enable) begin
-               if (valid_reg) begin         // New button press, so (re-)load count etc
+          else begin
+               if (valid) begin         // New button press, so (re-)load count etc
                     high_frequency_pwm_counter <= high_frequency_pwm_counter_init;
                     up_count <= 1'b1;
                end
                else if (vibrato & pwm_rose) begin
-                    high_frequency_pwm_counter <= (up_count) ? vib_upwards : vib_downwards;
+                    high_frequency_pwm_counter <= (up_count & enable) ? vib_upwards : vib_downwards;
                     if (high_frequency_pwm_counter == bound_up) up_count <= 1'b1;
                     else if (high_frequency_pwm_counter == bound_down) up_count <= 1'b0;
                end
