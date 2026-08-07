fed16cec9061a2c100744a8b8c1c2256d3741b23
aofarmakis <alfarmak@ad.inria.fr>
2026-08-07 15:13:59 +0200

    fix: effects color only on last note

--- macros/heichips26_daftASIC/rtl/top_module.v
+++ macros/heichips26_daftASIC/rtl/top_module.v
@@ -114,7 +114,7 @@ module top_module (reset, pixel_clk, ps2clk, ps2data, hsync, vsync, red, green, 
                     if (vibrato & staccato) rgb_out = ~note_serial_out ? {3{static_part}} : 3'b110 & {3{note_serial_out}};
                     else if (vibrato) rgb_out = ~note_serial_out ? {3{static_part}} : 3'b101 & {3{note_serial_out}};
                     else if (staccato) rgb_out = ~note_serial_out ? {3{static_part}} : 3'b011 & {3{note_serial_out}};
-                    else rgb_out = {3{static_part}};
+                    else rgb_out = ~note_serial_out ? {3{static_part}} : {3{note_serial_out}};
                end
                else rgb_out = ~note_serial_out ? {3{static_part}} : {3{note_serial_out}};
           end
